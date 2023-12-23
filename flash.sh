9b9ada7ce3eba93c4d3ebbcf1630c36a2df772e4
polty <polt.gun@gmail.com>
2023-12-23 11:11:13 +0100

    fix bluetooth profiles

--- flash.sh
+++ flash.sh
@@ -11,12 +11,14 @@ echo -n "[L/r]"
 read
 if [[ $REPLY == "r" ]]; then
 	SIDE=right
+	echo right chosen
 else
 	SIDE=left
 fi
 
 while true
 do
-	sudo mount /dev/"$DEV" /tmp/nicenano && sudo cp "$SIDE".uf2 /tmp/nicenano/ && exit 0
+	sudo mount --mkdir /dev/"$DEV" /tmp/nicenano && sudo cp "$SIDE".uf2 /tmp/nicenano/ && exit 0
+	lsblk | grep disk
 	sleep 2
 done
